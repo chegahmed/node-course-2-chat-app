76aaff77860dc13b51312ad9a43b979e99694c55
Ahmed chega <chegahemd@gmail.com>
2017-04-01 18:45:29 +0000

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -25,6 +25,11 @@ io.on('connection', (socket) => {
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        io.emit('newMessage',{
+            from: message.form,
+            text: message.text,
+            createAt :new Date().getTime()
+        })
     });
 
 
